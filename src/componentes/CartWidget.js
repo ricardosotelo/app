1134376d473e21475fbde6281a0f8073836402e8
ricardosn <ricardoosn@gmail.com>
2022-05-17 19:43:16 -0300

    nav bar

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,15 @@
+
+
+
+import CartWidget from './icon.svg';
+
+
+function icon () {
+return (
+    <button type="button" class="btn btn-success">
+    <img src={CartWidget} class="icon" alt="logo" /> Add to cart</button>
+    
+)
+
+}
+export default icon;
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,19 @@
+import CartWidget from './CartWidget';
+import './NavBar.css';
+function Navbar () {
+
+return(
+    <ul>
+    <li>home</li>  
+    <li>img</li>  
+    <li>vids</li>
+    <li>contact</li>
+    <CartWidget></CartWidget>
+    </ul>
+
+    
+)
+
+}
+
+export default Navbar;
